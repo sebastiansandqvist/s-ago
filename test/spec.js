f2d00f91d901fd67af2405bfad985899b4065366
Sebastian Sandqvist <seb@sparque.me>
2015-08-29 14:55:59 -0700

    first commit

---
+++ test/spec.js
@@ -0,0 +1,14 @@
+// ----- dependencies
+// ---------------------------------------
+require('blanket')({
+    pattern: function (filename) {
+        return !/node_modules/.test(filename);
+    }
+});
+
+var expect = require('chai').expect;
+var app = require('../index.js');
+
+
+// ----- tests
+// ---------------------------------------
