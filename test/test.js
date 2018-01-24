0b287d5851607700b3f909047597e3dc6e787be0
Sebastian Sandqvist <seb@sparque.me>
2018-01-24 10:44:10 -0800

    support old node versions in tests

--- test/test.js
+++ test/test.js
@@ -3,18 +3,18 @@ import ago from '../';
 
 const timestamp = new Date();
 
-test('just now', (t) => {
+test('just now', function(t) {
   t.is(ago(timestamp), 'just now');
 });
 
-test('minute', (t) => {
+test('minute', function(t) {
   const minute = new Date(timestamp - (60 * 1000));
   const minute2 = new Date(timestamp - ((60 * 1000) + 5000));
   t.is(ago(minute), 'a minute ago');
   t.is(ago(minute2), 'a minute ago');
 });
 
-test('minutes', (t) => {
+test('minutes', function(t) {
   const minutes = new Date(timestamp - (2 * 60 * 1000));
   const minutes2 = new Date(timestamp - (20 * 60 * 1000));
   const minutes3 = new Date(timestamp - (45 * 60 * 1000));
@@ -23,7 +23,7 @@ test('minutes', (t) => {
   t.is(ago(minutes3), '45 minutes ago');
 });
 
-test('hour', (t) => {
+test('hour', function(t) {
   const underHour = new Date(timestamp - (50 * 60 * 1000));
   const hour = new Date(timestamp - (60 * 60 * 1000));
   const hourPlus = new Date(timestamp - (90 * 60 * 1000));
@@ -32,7 +32,7 @@ test('hour', (t) => {
   t.is(ago(hourPlus), 'an hour ago');
 });
 
-test('hours', (t) => {
+test('hours', function(t) {
   const hours = new Date(timestamp - (2 * 60 * 60 * 1000));
   const hours2 = new Date(timestamp - (3.5 * 60 * 60 * 1000));
   const hours3 = new Date(timestamp - (19 * 60 * 60 * 1000));
@@ -41,7 +41,7 @@ test('hours', (t) => {
   t.is(ago(hours3), '19 hours ago');
 });
 
-test('yesterday', (t) => {
+test('yesterday', function(t) {
   const yesterday = new Date(timestamp - (20 * 60 * 60 * 1000));
   const yesterday2 = new Date(timestamp - (40 * 60 * 60 * 1000));
   const yesterday3 = new Date(timestamp - (47 * 60 * 60 * 1000));
@@ -50,28 +50,28 @@ test('yesterday', (t) => {
   t.is(ago(yesterday3), 'yesterday');
 });
 
-test('days', (t) => {
+test('days', function(t) {
   const days = new Date(timestamp - (2 * 24 * 60 * 60 * 1000));
   const days2 = new Date(timestamp - (5 * 24 * 60 * 60 * 1000));
   t.is(ago(days), '2 days ago');
   t.is(ago(days2), '5 days ago');
 });
 
-test('week', (t) => {
+test('week', function(t) {
   const week = new Date(timestamp - (6 * 24 * 60 * 60 * 1000));
   const week2 = new Date(timestamp - (13 * 24 * 60 * 60 * 1000));
   t.is(ago(week), 'last week');
   t.is(ago(week2), 'last week');
 });
 
-test('weeks', (t) => {
+test('weeks', function(t) {
   const weeks = new Date(timestamp - (2 * 7 * 24 * 60 * 60 * 1000));
   const weeks2 = new Date(timestamp - (3 * 7 * 24 * 60 * 60 * 1000));
   t.is(ago(weeks), '2 weeks ago');
   t.is(ago(weeks2), '3 weeks ago');
 });
 
-test('month', (t) => {
+test('month', function(t) {
   const month = new Date(timestamp - (4 * 7 * 24 * 60 * 60 * 1000));
   const month2 = new Date(timestamp - (30 * 24 * 60 * 60 * 1000));
   const month3 = new Date(timestamp - (50 * 24 * 60 * 60 * 1000));
@@ -80,21 +80,21 @@ test('month', (t) => {
   t.is(ago(month3), 'last month');
 });
 
-test('months', (t) => {
+test('months', function(t) {
   const months = new Date(timestamp - (2 * 30 * 24 * 60 * 60 * 1000));
   const months2 = new Date(timestamp - (10 * 30 * 24 * 60 * 60 * 1000));
   t.is(ago(months), '2 months ago');
   t.is(ago(months2), '10 months ago');
 });
 
-test('year', (t) => {
+test('year', function(t) {
   const year = new Date(timestamp - (350 * 24 * 60 * 60 * 1000));
   const year2 = new Date(timestamp - (1.5 * 360 * 24 * 60 * 60 * 1000));
   t.is(ago(year), 'last year');
   t.is(ago(year2), 'last year');
 });
 
-test('years', (t) => {
+test('years', function(t) {
   const years = new Date(timestamp - (2 * 365 * 24 * 60 * 60 * 1000));
   const years2 = new Date(timestamp - (20 * 365 * 24 * 60 * 60 * 1000));
   const years3 = new Date(timestamp - (100 * 365 * 24 * 60 * 60 * 1000));
