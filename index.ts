44d51c43e0d1cd8ce5a8eacc8aa4e673d930a7a2
Arthur Tyukayev <arthur@fullstacklabs.co>
2019-08-09 22:45:02 -0700

    Added future time display

--- index.ts
+++ index.ts
@@ -14,21 +14,24 @@ ago(hoursAgo); // '6 hours ago'
 
 */
 
-function format(diff: number, divisor: number, unit: string, prev: string) {
+function format(diff: number, divisor: number, unit: string, prev: string, inv: string, inverted: boolean) {
   var val = Math.round(diff / divisor);
+  if (inverted) {
+      return val <= 1 ? inv : 'in ' + val + ' ' + unit + 's';
+  }
   return val <= 1 ? prev : val + ' ' + unit + 's ago';
 }
 
 const units = [
-  { max: 2760000, value: 60000, name: 'minute', prev: 'a minute ago' }, // max: 46 minutes
-  { max: 72000000, value: 3600000, name: 'hour', prev: 'an hour ago' }, // max: 20 hours
-  { max: 518400000, value: 86400000, name: 'day', prev: 'yesterday' }, // max: 6 days
-  { max: 2419200000, value: 604800000, name: 'week', prev: 'last week' }, // max: 28 days
-  { max: 28512000000, value: 2592000000, name: 'month', prev: 'last month' } // max: 11 months
+  { max: 2760000, value: 60000, name: 'minute', prev: 'a minute ago', in: 'in a minute' }, // max: 46 minutes
+  { max: 72000000, value: 3600000, name: 'hour', prev: 'an hour ago', in: 'in an hour' }, // max: 20 hours
+  { max: 518400000, value: 86400000, name: 'day', prev: 'yesterday', in: 'in a day' }, // max: 6 days
+  { max: 2419200000, value: 604800000, name: 'week', prev: 'last week', in: 'in a week' }, // max: 28 days
+  { max: 28512000000, value: 2592000000, name: 'month', prev: 'last month', in: 'in a month' } // max: 11 months
 ];
 
 export default function ago(date: Date): string {
-
+  const inverted = Date.now() < date.getTime()
   const diff = Math.abs(Date.now() - date.getTime());
 
   // less than a minute
@@ -36,7 +39,7 @@ export default function ago(date: Date): string {
 
   for (var i = 0; i < units.length; i++) {
     if (diff < units[i].max) {
-      return format(diff, units[i].value, units[i].name, units[i].prev);
+      return format(diff, units[i].value, units[i].name, units[i].prev, units[i].in, inverted);
     }
   }
 
@@ -48,6 +51,6 @@ export default function ago(date: Date): string {
   //    name: 'year',
   //    prev: 'last year'
   //  }
-  return format(diff, 31536000000, 'year', 'last year');
+  return format(diff, 31536000000, 'year', 'last year', 'in a year', inverted);
 
 }
